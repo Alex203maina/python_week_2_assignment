cc1104be30b04a22a6b7a7b4eae44c2c64c84c6a
Alex203maina <alex203maina@gmail.com>
2024-11-08 20:50:43 +0300

    assignment done

---
+++ answer.py
@@ -0,0 +1,26 @@
+
+# question 1
+my_list = []
+
+# question 2
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# question 3
+my_list.insert(2, 15)
+
+# question 4
+my_list1 = [50,60, 70]
+my_list.extend(my_list1)
+
+# question 5
+my_list.remove(70)
+
+# question 6
+my_list.sort()
+
+# question 7
+index_30 = my_list.index(30)
+print(index_30)
